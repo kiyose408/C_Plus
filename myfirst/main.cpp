6a5123c15296608f6f433f9a2f39fd298ad52d1c
kiyose408 <2826226586@qq.com>
2023-04-06 18:10:32 +0800

    myfirst

---
+++ myfirst/main.cpp
@@ -0,0 +1,10 @@
+//myfirst c plus program -- disply a message
+#include<iostream>                          //a PREPROCESSOR directive
+int main()                                  //funtion header
+{                                           //start of funtion body
+    using namespace std;                    //make definitions visble
+    cout <<"Come up and C++ me somr time."; //message
+    cout <<endl;                            //start a new line
+    cout <<"You won't regret it!"<<endl ;    //more output
+    return 0;       //terminate main()
+}
