dbf1b50b3bea39aea85922a8d1f9a396ecd70a8a
xuhui <xuhui20001028@163.com>
2023-04-09 13:03:32 +0800

    notebook computer first connectting

---
+++ hello，c++/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//this program is a clasical simple to learn c++.
+using namespace std;
+
+int main()
+{
+    cout << "Hello C++!"<< endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
