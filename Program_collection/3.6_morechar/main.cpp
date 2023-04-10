53ae1e4eb9c8f33c44ab7b0c478a13ba093291f5
kiyose408 <2826226586@qq.com>
2023-04-10 15:36:27 +0800

    3.6_morechar

---
+++ Program_collection/3.6_morechar/main.cpp
@@ -0,0 +1,23 @@
+//page48
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char ch = 'M';      //assignn ASCII code for M to ch
+    int i = ch;             //store same code in an int
+    cout << "The ASCII code for" <<ch<< "is " << i <<endl;//值的类型将引导cout输出选择如何显示值
+
+    cout << "Add one to the character code: " << endl;
+    ch = ch+1;
+    i = ch;
+    cout << "The ASCII code for " <<ch << " is " << i <<endl;
+
+    //using the cout.put() member function to display a char
+    cout << "Displaying char ch using cout.put(ch):";
+    cout.put(ch);
+
+    //using cout.put() to display a char constant
+    cout.put('!');
+    cout << endl << "Done" <<endl;
+    return 0;
+}
