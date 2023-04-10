243122abec01a206a8bba9cef79ceaa0742f2109
kiyose408 <2826226586@qq.com>
2023-04-10 15:25:23 +0800

    3.5_chartype

---
+++ Program_collection/3.5_Chartype/main.cpp
@@ -0,0 +1,12 @@
+//page 48
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char ch;            //declare a char variable//声明一个char变量
+    cout  << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << "character . "<< endl;
+    return 0;
+}
