791b59645310c910e70524ae75c75b4c91362164
kiyose408 <2826226586@qq.com>
2023-04-18 02:40:07 +0800

    4.15_pointer

---
+++ Program_collection/4.15_pointer/main.cpp
@@ -0,0 +1,22 @@
+//P99:our first pointer variable
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int updates = 6;
+    int *p_updates;
+    p_updates =&updates;
+
+    //express values two ways
+    cout << "Values: updates = "<<updates;
+    cout << ", *p_updates = "<<*p_updates<<endl;
+
+    //express address two ways
+    cout << "Addresss: &updates = "<<&updates;
+    cout << ", p_updates = "<<p_updates<<endl;
+
+    //use pointer to change value
+    *p_updates = *p_updates +1;
+    cout <<"Now updates = "<<updates <<endl;
+    return 0;
+}
