83bfbb9e102812c68e2f51078066b7f31498fe77
kiyose408 <2826226586@qq.com>
2023-04-30 01:14:25 +0800

    first commit in vs

---
+++ Program_collection/4.21_newstrct/4.21_newstrct/4.21_newstrct.cpp
@@ -0,0 +1,24 @@
+﻿//P115:using new with a structure
+#include <iostream>
+struct inflatable		//structure definition
+{
+	char name[20];
+	float volume;
+	double price;
+};
+int main()
+{
+	using namespace std;
+	inflatable* ps = new inflatable;	//allot memory for structure
+	cout << "Enter name for inflatable item: ";
+	cin.get(ps->name, 20);
+	cout << "Enter volume in cubic feet: ";
+	cin >> (*ps).volume;
+	cout << "Enter price: $";
+	cin >> ps->price;
+	cout << "Name: " << (*ps).name << endl;
+	cout << "Volume: " << ps->volume << " cubic feet \n";
+	cout << "Price: $" << ps->price << endl;
+	delete ps;
+	return 0;
+}
