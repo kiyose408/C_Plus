64fe06e556ff72639e49a0aaa45c2616c5ecc021
kiyose408 <2826226586@qq.com>
2023-04-30 01:36:19 +0800

    4.22_delete

---
+++ Program_collection/4.22_delete.cpp/4.22_delete.cpp/4.22_delete.cpp.cpp
@@ -0,0 +1,29 @@
+﻿//P116:usinf the delete operator
+#include <iostream>
+#include <cstring>	//or string.h
+#pragma warning(disable:4996)		//防止strcpy在vs中安全性报错
+using namespace std;
+char* getname(void);		//function prototype
+int main()
+{
+	char* name;
+
+	name = getname();
+	cout << name << " at " << (int*)name << "\n";
+	delete [] name;
+
+	name = getname();
+	cout << name << " at " << (int*)name << "\n";
+	delete[] name;
+	return 0;
+}
+char* getname()
+{
+	char temp[80];
+	cout << "Enter last name: ";
+	cin >> temp;
+	char* pn = new char[strlen(temp) + 1];
+	strcpy(pn, temp);
+
+	return pn;
+}
