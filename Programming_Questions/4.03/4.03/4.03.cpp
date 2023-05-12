3acb0e829fe80eb0e02dc2aa0ed44fe35d7844cc
kiyose408 <2826226586@qq.com>
2023-05-12 23:58:34 +0800

    4.03

---
+++ Programming_Questions/4.03/4.03/4.03.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<cstring>
+int main()
+{
+	char fn[20];
+	char ln[20];
+	std::cout << "Enter your first name: ";
+	std::cin >> fn;
+	std::cout << "Enter your last name: ";
+	std::cin >> ln;
+	char name[50];
+	strcpy_s(name, fn);
+	strcat_s(name, ",");
+	strcat_s(name, ln);
+	std::cout << "Here is the information in a single string : " << name;
+	return 0;
+}
