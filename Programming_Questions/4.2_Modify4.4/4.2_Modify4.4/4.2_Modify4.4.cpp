ac14bf3ce10867a4241fb4e3df6c3c39772d0891
kiyose408 <2826226586@qq.com>
2023-05-12 23:38:21 +0800

    4.2_Modify4.4

---
+++ Programming_Questions/4.2_Modify4.4/4.2_Modify4.4/4.2_Modify4.4.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<string>
+int main()
+{
+	using namespace std;
+	const int ArSize = 20;
+	string name;
+	string dessert;
+	std::cout << "Enter your name:\n";
+	getline(cin, name);
+	std::cout << "Enter your favorite dessert:\n";
+	getline(cin, dessert);
+	std::cout << "I have some delicious [" << dessert;
+	std::cout << "] for you , [" << name<< "].\n";
+	return 0;
+}
