1e0da5a52850c49415ca8c5243a8537f58912588
kiyose408 <2826226586@qq.com>
2023-05-13 11:17:29 +0800

    4.06

---
+++ Programming_Questions/4.06/4.06/4.06.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<string>
+struct CandyBar {
+	std::string brand;//品牌
+	float heavy;
+	int calorie;
+};
+int main()
+{
+	using namespace std;
+	CandyBar candys[3] = {
+		{"aerbeisi",10,200},
+		{"xufuji",20,400},
+		{"buerjia",30,500}
+	};
+	for (int i = 0; i < 3; i++) {
+		std::cout << "\nBrand:" << candys[i].brand;
+		std::cout << "\nHeavy:" << candys[i].heavy;
+		std::cout << "\nCalorie:" << candys[i].calorie;
+		std::wcout << "\n___________";
+	}
+	return 0;
+}
