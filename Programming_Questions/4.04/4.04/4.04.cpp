7cdfb9922f080b3fe2c0beb6e2153b6fc097e6ec
kiyose408 <2826226586@qq.com>
2023-05-13 10:55:24 +0800

    4.04

---
+++ Programming_Questions/4.04/4.04/4.04.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<string>
+int main()
+{
+	using namespace std;
+	string fn;
+	string ln;
+	std::cout << "Enter your first name: ";
+	std::cin >> fn;
+	std::cout << "Enter your last name: ";
+	std::cin >> ln;
+	string name;
+	name = fn + "," + ln;
+	std::cout << "Here is the information in a single string : " << name;
+	return 0;
+}
