9f826cca79ccb2a718664e762a8123d7a4fbb55c
kiyose408 <2826226586@qq.com>
2023-05-13 11:08:04 +0800

    4.05

---
+++ Programming_Questions/4.05/4.05/4.05.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<string>
+struct CandyBar{
+	std::string brand;//品牌
+	float heavy;	
+	int calorie;
+};
+int main()
+{
+	CandyBar snack{
+		"Mocha Munch",
+		2.3,
+		350
+	};
+	std::cout << "snack CandyBar:\n";
+	std::cout << "Brand:" << snack.brand;
+	std::cout << "\nHeavy:" << snack.heavy;
+	std::cout << "\nCalorie:" << snack.calorie;
+	return 0;
+
+}
