2fbd16abe8267a6433b09022ff04fa9a33a2c54a
kiyose408 <2826226586@qq.com>
2023-05-14 11:09:35 +0800

    4.09

---
+++ Programming_Questions/4.09/4.09/4.09.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+struct CandyBar {
+	std::string brand;//品牌
+	float heavy;
+	int calorie;
+};
+int main()
+{
+	using namespace std;
+	CandyBar *ca1 = new CandyBar;
+	CandyBar *ca2 = new CandyBar;
+	CandyBar *ca3 = new CandyBar;
+	CandyBar *cas[3] = { ca1,ca2,ca3 };
+	for (int i = 0; i < 3; i++) {
+		std::cin >> cas[i]->brand;
+		std::cin >> cas[i]->calorie;
+		std::cin >> cas[i]->heavy;
+	}
+	for (int i = 0; i < 3; i++) {
+		std::cout << "\nBrand:" << cas[i]->brand;
+		std::cout << "\nHeavy:" << cas[i]->heavy;
+		std::cout << "\nCalorie:" <<cas[i]->calorie;
+		std::wcout << "\n___________";
+	}
+	delete ca1, ca2, ca3,cas;
+	return 0;
+}
