e2f8aabb9a38b65fc00b42b4ffd6700d128499e0
kiyose408 <2826226586@qq.com>
2023-05-15 00:45:40 +0800

    倒数退出（0做判断体）

---
+++ Program_collection/5.02_num_test/5.02_num_test/5.02_num_test.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+int main()
+{
+	using namespace std;
+	cout << "Enter the starting vountdown value: ";
+	int limit;
+	cin >> limit;
+	int i;
+	for (i = limit; i; i--)		//当i为0时中间的条件判断就是0也就是假（不成立）的情况，就会跳出循环
+		cout << "i = " << i << "\n";
+	cout << "Done now that i = " << i << "\n";
+	return 0;
+}
