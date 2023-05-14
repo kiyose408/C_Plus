ea8ecb4ea22395840f2c314b9a0f35c241b7ae30
kiyose408 <2826226586@qq.com>
2023-05-14 10:32:21 +0800

    4.08

---
+++ Programming_Questions/4.08/4.08/4.08.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<string>
+struct Pizza {
+	std::string Company;
+	float diameter;
+	float weight;
+};
+int main()
+{
+	Pizza *pizza = new Pizza;
+	std::cout << "\ndiameter:";
+	std::cin >> pizza->diameter;
+	std::cout << "\ncompany:";
+	std::cin >> pizza->Company;
+	std::cout << "\nweight:";
+	std::cin >> pizza->weight;
+	std::cout << "pizza信息为：";
+	std::cout << "\n公司:" << pizza->Company;
+	std::cout << "\n直径:" << pizza->diameter << "cm";
+	std::cout << "\n重量:" << pizza->weight << "g";
+	delete pizza;
+	return 0;
+}
