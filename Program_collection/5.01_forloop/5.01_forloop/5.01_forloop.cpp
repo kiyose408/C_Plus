bd0784e47d26a1a8ccb264b6b4907c94e7f5a4e6
kiyose408 <2826226586@qq.com>
2023-05-15 00:36:14 +0800

    5.01_forloop

---
+++ Program_collection/5.01_forloop/5.01_forloop/5.01_forloop.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+int main() 
+{
+	using namespace std;
+	int i;		//创建一个计数标识
+	//for(初始化；测试/比对条件;更新/继续执行)
+	for (i = 0; i < 5; i++)
+		cout << "C++ knows loops.\n";
+	cout << "C++ knows when to stop.\n";
+	return 0;
+}
