0c34d7e7f0c93659aa0ae8b12c989fb4251daeec
kiyose408 <2826226586@qq.com>
2023-05-16 12:23:08 +0800

    5.01自动求和

---
+++ Programming_Questions/5.01/5.01/5.01.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>//实现输入两个整数，然后将两整数之间所有数字自动求和返回，包括这两个数字
+int main()
+{
+	using namespace std;
+	int a, b, min, max, sum = 0;
+	std::cout << "Enter a num:";
+	std::cin >> a;
+	std::cout << "Enter a num:";
+	std::cin >> b;
+	if (a > b)
+	{
+		max = a;
+		min = b;
+	}
+	else
+	{
+		max = b;
+		min = a;
+	}
+	for (int i = min; i <= max; i++)
+	{
+		sum += i;
+	}
+	std::cout << "All num added that between " << min << " and " << max << " is " << sum;
+	return 0;
+}
