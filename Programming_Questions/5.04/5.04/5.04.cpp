0928d1cc2b457c90349c7e46b70caa8643ad3b5d
kiyose408 <2826226586@qq.com>
2023-05-20 12:17:53 +0800

    投资比较

---
+++ Programming_Questions/5.04/5.04/5.04.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+int main()
+{
+	using namespace std;
+	float De=100,Cl=100,mo=100,y=1;
+	for (; De >= Cl; )
+	{
+		De += 0.1*mo;
+		Cl = 1.05 * Cl;
+		y++;
+	}
+	std::cout << "第" << y << "年时，Cleo的收益将超过Dephne.\nCleo总持有投资金额: " << Cl << "\nDephne总持有投资金额： " << De << endl;
+}
